315779cd195e245a66577de6d4b8263b3a6188c0
김학영 <gkrdud5698@naver.com>
2021-11-22 23:54:22 +0900

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="foot-wrapper py-3 text-center text-muted">© 2021. Kim Hakyoung. All pictures cannot be copied without permission.</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.foot-wrapper {
+    border-top: 1px gray solid;
+}
+</style>
